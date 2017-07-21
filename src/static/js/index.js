299695b18d68724f60e7dd4bcfd71236659b6b76
Andrew Cameron <andrew.cameron@protonmail.com>
2017-07-21 16:41:36 +0100

    Refactoring. Dragging either origin or destination marker now triggers handleDragEnd()

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,75 +1,6 @@
 const SERVER_URL = 'http://127.0.0.1:5000'
 
-function getStops(origin, destination) {
-    var xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = () => {
-        if (xhr.status === 200 && xhr.readyState === 4) {
-            showJourneys(JSON.parse(xhr.responseText))
-        }
-    }
-    xhr.open('POST', SERVER_URL + "/getstops", true)
-    xhr.setRequestHeader('Content-Type', 'Application/json')
-    //xhr.send(JSON.stringify({origin: origin, destination: destination}))
-    xhr.send(JSON.stringify({
-        origin: origin,
-        destination: destination
-    }))
-}
-
-function showJourneys(journeys) {
-    console.log(journeys)
-    window.polylines = journeys.map(x => new google.maps.Polyline({
-        path: [
-            new google.maps.LatLng(x.board.coords[0],
-                x.board.coords[1]),
-            new google.maps.LatLng(x.deboard.coords[0],
-                x.deboard.coords[1])
-        ],
-        strokeColor: "#FF0000",
-        strokeOpacity: 1.0,
-        strokeWeight: 1,
-        map: window.map
-    }))
-
-    /*
-    var markers = []
-
-    for (var i = 0; i < journeys.length; i++) {
-        for (var j = 0; j < journeys[i].length; j++) {
-            var section = journeys[i][j]
-            markers.push(new google.maps.Marker({
-                position: {
-                    lat: section
-                }
-            }))
-        }
-    }
-    var markers = journeys.map(journey => {
-
-        new google.maps.Marker({
-            position: {
-                lat: x.coords[0],
-                lng: x.coords[1]
-            },
-            label: x.id,
-            map: window.map
-        })
-    })
-
-    zoomToBounds(markers)*/
-}
-
-
-function zoomToBounds(markers) {
-    console.log(markers)
-    var bounds = markers.reduce(
-        (bounds, marker) => bounds.extend(marker.getPosition()),
-        new google.maps.LatLngBounds())
-
-    window.map.fitBounds(bounds)
-}
-
-initMap = () => {
+function initMap() {
     const CENTER = {
         lat: 53.346348,
         lng: -6.263098
@@ -82,7 +13,7 @@ initMap = () => {
         }
     )
 
-    var originMarker = new google.maps.Marker({
+    window.originMarker = new google.maps.Marker({
         position: {
             lat: 53.346348,
             lng: -6.263098
@@ -92,7 +23,7 @@ initMap = () => {
         draggable: true
     })
 
-    var destinationMarker = new google.maps.Marker({
+    window.destinationMarker = new google.maps.Marker({
         position: {
             lat: 53.344196,
             lng: -6.257776
@@ -102,31 +33,84 @@ initMap = () => {
         draggable: true
     })
 
-    destinationMarker.addListener('dragend', event => {
-        if (typeof window.polylines !== 'undefined') {
-            for (var i = 0; i < window.polylines.length; i++) {
-                window.polylines[i].setMap(null)
-            }
-            window.polylines = []
-        }
-        var origin = {
-            lat: originMarker
-                .getPosition()
-                .lat(),
-            lng: originMarker
-                .getPosition()
-                .lng()
-        }
-        var destination = {
-            lat: destinationMarker
-                .getPosition()
-                .lat(),
-            lng: destinationMarker
-                .getPosition()
-                .lng()
-        }
-
-
-        getStops(origin, destination)
-    })
+    destinationMarker.addListener('dragend', handleDragEnd)
+    originMarker.addListener('dragend', handleDragEnd)
 }
+
+
+function handleDragEnd(event) {
+    if (typeof window.polylines !== 'undefined') {
+        for (var i = 0; i < window.polylines.length; i++) {
+            window.polylines[i].setMap(null)
+        }
+        window.polylines = []
+    }
+
+    var origin = {
+        lat: window.originMarker
+            .getPosition()
+            .lat(),
+        lng: window.originMarker
+            .getPosition()
+            .lng()
+    }
+
+    var destination = {
+        lat: window.destinationMarker
+            .getPosition()
+            .lat(),
+        lng: window.destinationMarker
+            .getPosition()
+            .lng()
+    }
+
+    getStops(origin, destination)
+}
+
+
+function getStops(origin, destination) {
+
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = () => {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            showJourneys(JSON.parse(xhr.responseText))
+        }
+    }
+
+    xhr.open('POST', SERVER_URL + "/getstops", true)
+    xhr.setRequestHeader('Content-Type', 'Application/json')
+
+    xhr.send(JSON.stringify({
+        origin: origin,
+        destination: destination
+    }))
+}
+
+
+function showJourneys(journeys) {
+    console.log(journeys)
+
+    window.polylines = journeys.map(x => new google.maps.Polyline({
+        path: [
+            new google.maps.LatLng(x.board.coords[0],
+                x.board.coords[1]),
+            new google.maps.LatLng(x.deboard.coords[0],
+                x.deboard.coords[1])
+        ],
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 1,
+        map: window.map
+    }))
+}
+
+/*
+function zoomToBounds(markers) {
+    console.log(markers)
+    var bounds = markers.reduce(
+        (bounds, marker) => bounds.extend(marker.getPosition()),
+        new google.maps.LatLngBounds())
+
+    window.map.fitBounds(bounds)
+}
+*/
